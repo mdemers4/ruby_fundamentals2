8c047dcf0cb5a8955a31055efce4ccb7581bfa40
mitchell demers <mitchelldemers66@gmail.com>
2016-08-09 12:04:27 -0400

    rice was added to the list in a method

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,10 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-for grocery in grocery_list
-	puts "*#{grocery}"
+def groceries(grocery_list)
+	grocery_list << "rice"
+	for grocery in grocery_list
+		puts "*#{grocery}"
+	end
 end
+
+groceries(grocery_list)
