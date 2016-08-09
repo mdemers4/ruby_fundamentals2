731ef609ae06caf73d653a449ef6d75adc4e2b2e
mitchell demers <mitchelldemers66@gmail.com>
2016-08-09 12:19:15 -0400

    searches the list to prompt you if you need bananas

--- exercise6.rb
+++ exercise6.rb
@@ -1,10 +1,24 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
+grocery_list << "rice"
 def groceries(grocery_list)
-	grocery_list << "rice"
+	
 	for grocery in grocery_list
 		puts "*#{grocery}"
+		if grocery == 'bananas'
+			bananas = true
+		end
+		
 	end
+	
+	puts grocery_list.length
+
+	if bananas == true
+		puts "you do not need bananas"
+	else
+		puts "you need bananas"
+	end
+
 end
 
 groceries(grocery_list)
+
