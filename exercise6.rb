f902d55f5e0330d76b089d1acef0e72ba13b1ec4
mitchell demers <mitchelldemers66@gmail.com>
2016-08-09 23:55:35 -0400

    sorted the list of groceries as well as removed salmon

--- exercise6.rb
+++ exercise6.rb
@@ -1,15 +1,17 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list << "rice"
+
 def groceries(grocery_list)
 	
 	for grocery in grocery_list
 		puts "*#{grocery}"
 		if grocery == 'bananas'
-			bananas = true
+			bananas = true   # there is an easier way to search for bananas
+							# using grocery_list.include?("bananas")
 		end
 		
 	end
-	
+
 	puts grocery_list.length
 
 	if bananas == true
@@ -18,7 +20,15 @@ def groceries(grocery_list)
 		puts "you need bananas"
 	end
 
+	puts "this is the second item on the list: #{grocery_list[1]}"
+	puts "sorted list:"
+	puts grocery_list.sort
+	puts 
+	grocery_list.delete( "salmon" )
+	puts grocery_list
+
 end
+
 
 groceries(grocery_list)
 
