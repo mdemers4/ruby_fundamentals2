2ceff87798d047f52bfb8859ea4e7f636ac0ce0b
mitchell demers <mitchelldemers66@gmail.com>
2016-08-10 10:25:00 -0400

    working and manipulating hashes

---
+++ exercise7.rb
@@ -0,0 +1,19 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+students[:cohort4] = 43
+
+students.each do |key,value|
+	puts "#{key}: #{value} students"
+end
+
+array = students.keys
+puts array
+
+students.each do |key,value|
+	
+	puts (value * 1.05).round
+end
+
