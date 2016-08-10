13e774aac33fe4a6d6a1c7ba9836e2fc0ed79668
mitchell demers <mitchelldemers66@gmail.com>
2016-08-10 11:17:09 -0400

    added an is number function and aa get int to confirm that an integer is used

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,25 @@
+
+
+
+
+def is_number?(obj)
+	obj.to_s == obj.to_i.to_s
+end
+
+def get_int()
+	puts "enter a temperature in fahrenheit"
+	temp_in_fahrenheit = gets.chomp
+
+	if is_number?(temp_in_fahrenheit) == true 
+		return temp_in_fahrenheit
+	else
+		puts "not an integer"
+		get_int()
+	end
+end
+
+
+
 def fahrenheit_to_celsius (fahrenheit)
 	f = fahrenheit.to_i
 	c = (f -32) * 5 / 9
@@ -6,4 +28,21 @@ end
 
 fahrenheit_to_celsius ("99abc") #outputs 37
 fahrenheit_to_celsius ("103") #prints 39
-fahrenheit_to_celsius ("abc99") # prints -18 due to to_i only retreving from the start of a string
+fahrenheit_to_celsius ("abc99") # prints -18 due to to_i only retreving from the start of a string ie f = 0
+
+
+temp = get_int()
+fahrenheit_to_celsius (temp)
+
+# an array is an ordered set of 
+
+
+
+
+
+
+
+
+
+
+
